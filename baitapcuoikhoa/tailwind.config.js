91e036fe3a7081997abc5e24befc0120463699e5
Uy Đình <uygmdo@gmail.com>
2021-09-23 22:58:52 +0700

    update bai cuoi khoa dot 1

--- baitapcuoikhoa/tailwind.config.js
+++ baitapcuoikhoa/tailwind.config.js
@@ -12,10 +12,32 @@ module.exports = {
       'body': ["'Poppins', sans-serif"]
     },
     extend: {
+      fontSize: {
+        "h2": ["40px", {lineHeight: 1.5}],
+        "p-l": ["18px", {lineHeight: "28px"}],
+        "h3": ["32px", {lineHeight: 1.5}],
+        "h4": ["22px", {lineHeight: 1.5}],
+        "h5": ["20px", {lineHeight: 1.5}],
+        "h6": ["16px", {lineHeight: 1.5}],
+
+      },
       colors: {
         "light" : '#BCD0E5',
         "secondary": '#173A56',
-        "primary": '#02897A'
+        "primary": '#02897A',
+        "dark":{
+          "DEFAULT": '#1F2E35',
+          "800": '#22343D'
+        },
+        "yellow": {
+          1000: "#FF9900",
+      },
+      "red": {
+        1000: "#F03E3D",
+      }, 
+      "blue": {
+        1000: "#4D8DFF",
+      }, 
       },
     },
   },
